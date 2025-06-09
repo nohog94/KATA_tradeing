5cb080b3e4f6ed7f434764bb729107b381ba6354
sy.k <150867256+huihihet@users.noreply.github.com>
2025-06-09 12:45:40 +0900

    Update NemoStockBroker.java

--- src/main/java/NemoStockBroker.java
+++ src/main/java/NemoStockBroker.java
@@ -1,28 +1,43 @@
 public class NemoStockBroker implements StockBroker {
+    private static final int ONE_SECOND = 1000;
+    private static final String WRONG_CODE = "999999";
     private final NemoAPI nemoAPI = new NemoAPI();
 
     @Override
-    public boolean login(String ID, String Pass) {
-        nemoAPI.certification(ID, Pass);
+    public boolean login(String id, String password) {
+        nemoAPI.certification(id, password);
         return true;
     }
 
     @Override
-    public boolean buy(String stockCode, int price, int count) {
+    public boolean buy(String stockCode, int price, int quantity) {
         if (isIllegalStockCode(stockCode))
             throw new IllegalArgumentException();
+        if (isValidNumber(price, quantity))
+            throw new IllegalArgumentException();
 
-        nemoAPI.purchasingStock(stockCode, price, count);
+        nemoAPI.purchasingStock(stockCode, price, quantity);
         return true;
     }
 
     @Override
-    public boolean sell(String stockCode, int price, int count) {
+    public boolean sell(String stockCode, int price, int quantity) {
         if (isIllegalStockCode(stockCode))
             throw new IllegalArgumentException();
+        if (isValidNumber(price, quantity))
+            throw new IllegalArgumentException();
 
-        nemoAPI.sellingStock(stockCode, price, count);
+        nemoAPI.sellingStock(stockCode, price, quantity);
         return true;
+    }
+
+    private static boolean isValidNumber(int price, int quantity) {
+        return price == 0 || quantity == 0;
+    }
+
+    @Override
+    public int getPrice(String stockCode) {
+        return getPrice(stockCode, ONE_SECOND);
     }
 
     @Override
@@ -37,10 +52,6 @@ public class NemoStockBroker implements StockBroker {
         return nemoAPI.getMarketPrice(stockCode, minute);
     }
 
-    private static boolean isIllegalStockCode(String stockCode) {
-        return stockCode.equals("999999");
-    }
-
     @Override
     public boolean buyNiceTiming(String stockCode, int amount) {
         return false;
@@ -50,4 +61,9 @@ public class NemoStockBroker implements StockBroker {
     public boolean sellNiceTiming(String stockCode, int quantity) {
         return false;
     }
+
+    private static boolean isIllegalStockCode(String stockCode) {
+        return stockCode.equals(WRONG_CODE);
+    }
 }
+
