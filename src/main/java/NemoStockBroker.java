ade4c5dcdebdbec561dea8991be58b54e6d59a62
Jinchan <ghkdwlscks@gmail.com>
2025-06-09 10:26:52 +0900

    feature

---
+++ src/main/java/NemoStockBroker.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+
+public class NemoStockBroker implements StockBroker {
+    private final NemoAPI nemoAPI = new NemoAPI();
+
+    public boolean login(String ID, String Pass) {
+        nemoAPI.certification(ID, Pass);
+        return true;
+    }
+
+    public boolean buy(String stockCode, int price, int count) {
+        if (isIllegalStockCode(stockCode))
+            throw new IllegalArgumentException();
+
+        nemoAPI.purchasingStock(stockCode, price, count);
+        return true;
+    }
+
+    public boolean sell(String stockCode, int price, int count) {
+        if (isIllegalStockCode(stockCode))
+            throw new IllegalArgumentException();
+
+        nemoAPI.sellingStock(stockCode, price, count);
+        return true;
+    }
+
+    public int getPrice(String stockCode, int minute) {
+        if (isIllegalStockCode(stockCode))
+            throw new IllegalArgumentException();
+
+        return nemoAPI.getMarketPrice(stockCode, minute);
+    }
+
+    private static boolean isIllegalStockCode(String stockCode) {
+        return stockCode.equals("999999");
+    }
+}
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,8 +1,8 @@
 package test.java;
 
-import main.java.*;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
